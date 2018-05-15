1b494b290f125e44614d7807fca3be680ae53190
Niyazi Ozturk <nozt001@aucklanduni.ac.nz>
2018-05-15 15:57:00 +1200

    IProductRepository interface is created for Mock object in order to retrieve initial products without a database.

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -28,6 +32,15 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {
             // put the bindings here
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {   new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf board", Price = 179 },
+                new Product { Name = "Running shoes", Price = 95 },
+            });
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+
         }
     }
 }
